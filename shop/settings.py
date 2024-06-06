7960fef85dc294100c802fcd1cbfd4757bf1d70f
emmanuel <jackoliver023@gmail.com>
2024-06-06 17:06:13 +0100

    settings updated

--- shop/settings.py
+++ shop/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["eshop-v37y.onrender.com", "http://localhost:8000/", "http://127.0.0.1:8000/"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["eshop-v37y.onrender.com", "http://localhost:8000/", "http://127.0.0.1:8000/"]
+# ALLOWED_HOSTS = []
 
 
 # Application definition
